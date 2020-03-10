455469c4b612ad75620aeea1e1ee541f79eeea72
Anil Gauda <x18180663@student.ncirl.ie>
2020-03-10 15:02:50 +0000

    Changed to Spring Boot Project.

--- app/common/trade/Share.scala
+++
@@ -1,5 +0,0 @@
-package common.trade
-
-case class Share(name: String,tickerCode: String,price: String,avgPrice50Percent: String
-                   ,avgPrice200Percent: String, askValue:String,bid: String, prevClose: String,
-                 eps: String,pe: String, annDate: String,priceHistory: Any,dividendHistory: Any,currency: String )
